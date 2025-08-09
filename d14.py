eebd5ef930772ea68e9feff08aadca951b6c70cb
Alister McKinley <alpatine@gmail.com>
2025-08-09 13:16:02 +1000

    Solve day 14

---
+++ d14.py
@@ -0,0 +1,53 @@
+import re
+from collections import defaultdict, namedtuple
+
+import aoc
+
+# Common
+Reindeer = namedtuple('Reindeer', 'name, speed, fly_time, rest_time')
+
+def parse_data(data: str) -> list[Reindeer]:
+    reindeer = list()
+    for line in data.splitlines():
+        name, speed, fly_time, rest_time = re.match(r'(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.', line).group(1, 2, 3, 4)
+        reindeer.append(Reindeer(name, int(speed), int(fly_time), int(rest_time)))
+
+    return reindeer
+
+def calculate_distance(time_passed: int, speed: int, fly_time: int, rest_time: int) -> int:
+    total_time = fly_time + rest_time
+    cycles, remainder = divmod(time_passed, total_time)
+    distance = speed * (cycles * fly_time + min(remainder,  fly_time))
+    return distance
+
+# Part 1
+def p1(data: str, time_passed: int = 2503) -> int:
+    all_reindeer = parse_data(data)
+    distances = list()
+    for reindeer in all_reindeer:
+        distances.append(calculate_distance(time_passed, *reindeer[1:]))
+    
+    return max(distances)
+
+# Part 2
+def p2(data: str, time_passed: int = 2503) -> int:
+    all_reindeer = parse_data(data)
+    scores: dict[str, int] = defaultdict(int)
+    for time in range(1, time_passed + 1):
+        distances: dict[int, list[str]] = defaultdict(list)
+        max_dist = 0
+        for reindeer in all_reindeer:
+            distance = calculate_distance(time, *reindeer[1:])
+            if distance >= max_dist:
+                distances[distance].append(reindeer.name)
+                max_dist = distance
+        for name in distances[max_dist]:
+            scores[name] += 1
+    
+    return max(scores.values())
+
+# Main
+if __name__ == '__main__':
+    data = aoc.d14data()
+    print(p1(data))
+    print(p2(data))
---
+++ d14_test.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+import aoc
+from d14 import p1, p2
+
+
+class D14_Test(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.given_data = aoc.d14data()
+
+    # Part 1 Tests
+    def test_p1_examples(self):
+        data = """Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."""
+        self.assertEqual(p1(data, 1), 16)
+        self.assertEqual(p1(data, 10), 160)
+        self.assertEqual(p1(data, 11), 176)
+        self.assertEqual(p1(data, 12), 176)
+        self.assertEqual(p1(data, 1000), 1120)
+    
+    def test_p1(self):
+        self.assertEqual(p1(self.given_data), 2660)
+
+    # Part 2 Tests
+    def test_p2_examples(self):
+        data = """Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."""
+        self.assertEqual(p2(data, 1), 1)
+        self.assertEqual(p2(data, 140), 139)
+        self.assertEqual(p2(data, 1000), 689)
+    
+    def test_p2(self):
+        self.assertEqual(p2(self.given_data), 1256)
+
